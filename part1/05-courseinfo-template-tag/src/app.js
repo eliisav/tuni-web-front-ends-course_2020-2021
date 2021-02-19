d0c7c6518e15e1e979010c119f7ca09136f0edf9
Timo Mäkinen <timo.makinen@tuni.fi>
2021-02-19 22:37:31 +0200

    add /part1/05-courseinfo-template-tag

---
+++ part1/05-courseinfo-template-tag/src/app.js
@@ -0,0 +1,13 @@
+
+// ------------------------------------------------------------ //
+// ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
+// ------------------------------------------------------------ //
+const commitSHA = 'commit-sha-in-here';   //
+// ------------------------------------------------------------ //
+
+const App = ({data, template}) => {
+
+
+  return document.createTextNode("hello courseinfo template tag");
+
+};
