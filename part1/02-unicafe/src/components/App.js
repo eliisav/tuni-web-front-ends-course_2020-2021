8237dc292fbd3cce082d40175884f511e1257329
Timo Mäkinen <timo.makinen@tuni.fi>
2021-02-21 12:38:45 +0200

    add /part1/02-unicafe

---
+++ part1/02-unicafe/src/components/App.js
@@ -0,0 +1,19 @@
+
+import { useState } from 'react';
+
+// ------------------------------------------------------------ //
+// ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
+// ------------------------------------------------------------ //
+export const commitSHA = 'commit-sha-in-here';
+// ------------------------------------------------------------ //
+
+
+
+
+export const App = () => {
+
+  
+  return (
+    <div>unicafe</div>
+  )
+}
