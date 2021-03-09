4b6b84b1c3977d943a8774e386bad754e0dd3a27
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-03-09 23:30:53 +0200

    Add courseinfo without react

--- round01/04-courseinfo-without/src/myact.js
+++ round01/04-courseinfo-without/src/myact.js
@@ -2,17 +2,34 @@
 const Myact = {
 
   createElement: function (tag, props, ...childNodes) {
+    let element;
 
+    if (typeof tag === 'function') {
+      element = tag(props)
+    } else {
+      element = document.createElement(tag)
+    }
 
+    element.append(...childNodes)
+    console.log(element)
+    return element
   }
-  
+
 }
 
 const MyactDOM = {
 
   render: function (element, parent) {
+    console.log(element)
+    console.log(parent)
 
+    // Delete the old contents of the parent
+    while (parent.firstChild) {
+      parent.removeChild(parent.firstChild)
+    }
     
+    // Put a new element inside the parent
+    parent.append(element)
   }
 
 }
