152a732a3819e92e43b03656bce5e77b513e2673
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-03-09 14:43:05 +0200

    Add courseinfo without jsx

--- round01/04-courseinfo-without/src/app.js
+++ round01/04-courseinfo-without/src/app.js
@@ -7,12 +7,45 @@
 const commitSHA = 'commit-sha-in-here';   //
 // ------------------------------------------------------------ //
 
-const Header = props => null;
+const Header = props => {
+    console.log(props)
+    return React.createElement(
+        'h1', null, props
+    )
+};
 
-const Part = props => null;
+const Part = props => {
+    console.log(props)
+    return React.createElement(
+      'p', null, props.name, ' ', props.exercises
+    )
+};
 
-const Content = props => null;
+const Content = props => {
+    console.log(props)
+    const parts = props.map(part => Part(part))
+    return React.createElement(
+        'div', null, ...parts
+    )
+};
 
-const Total = props => null;
+const Total = props => {
+    console.log(props)
+    let total = 0
+    props.forEach(part => {
+      total += part.exercises
+    })
+    return React.createElement(
+      'p', null, 'Number of exercises ', total
+    )
+};
 
-const App = ({ course }) => null;
+const App = ({ course }) => {
+    return React.createElement(
+        'div',
+        null,
+        Header(course.name),
+        Content(course.parts),
+        Total(course.parts)
+    )  
+};
