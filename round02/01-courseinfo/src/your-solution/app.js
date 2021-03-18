962b1a46f8efa8ef48b6b6927458420c9a8e7581
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-03-19 00:09:11 +0200

    Add courseinfo step 6

--- round02/01-courseinfo/src/your-solution/app.js
+++ round02/01-courseinfo/src/your-solution/app.js
@@ -6,11 +6,74 @@ export const commitSHA = 'commit-sha-in-here';
 // ------------------------------------------------------------ //
 
 
-export const  App = () => {
+const Header = (props) => {
+  console.log(props)
   return (
-    <div>
-        course information
-    </div>
-  );
+    <h1>{props.course}</h1>
+  )
 }
 
+const Part = (props) => {
+  console.log(props)
+  return (
+    <p>
+      {props.part.name} {props.part.exercises}
+    </p>
+  )
+}
+
+const Content = ({parts}) => {
+  return parts.map(part => <Part key={part.id} part={part} />)
+}
+
+const Total = (props) => {
+  console.log(props)
+  let total = 0
+  props.parts.forEach(part => {
+    total += part.exercises
+  })
+  return (
+    <p>Number of exercises {total}</p>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+      <Header course={course.name} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    id: 1,
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10,
+        id: 1
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7,
+        id: 2
+      },
+      {
+        name: 'State of a component',
+        exercises: 14,
+        id: 3
+      },
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course} />
+    </div>
+  )
+
+}
