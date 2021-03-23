e48d2f2fea21bf399aa09c9b00f2f132df5df037
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-03-23 21:00:22 +0200

    Add phonebook-server exercises 2.11 and 2.15-2.18

--- round02/03-phonebook-server/src/your-solution/app.js
+++ round02/03-phonebook-server/src/your-solution/app.js
@@ -1,3 +1,7 @@
+import React, { useState, useEffect } from 'react'
+import personService from './person-service'
+import {Filter, PersonForm, Persons} from './components'
+
 
 // ------------------------------------------------------------ //
 // ENTER COMMIT SHA OF YOUR REPO IN HERE                        //
@@ -7,10 +11,94 @@ export const commitSHA = 'commit-sha-in-here';
 
 
 export const App = () => {
+  const [ contacts, setContacts ] = useState([])
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ filter, setFilter ] = useState('')
+
+  useEffect(() => {
+    personService
+      .getAll()
+      .then(persons => {
+        setContacts(persons)
+      })
+  }, [])
+
+  const addContact = (event) => {
+    event.preventDefault()
+
+    const personFound = contacts.find(person => person.name === newName)
+
+    if (personFound === undefined) {
+      const newContact = {
+        name: newName,
+        number: newNumber
+      }
+      
+      personService
+        .create(newContact)
+        .then(returnedPerson => {
+          setContacts(contacts.concat(returnedPerson))
+        })
+
+    } else if (window.confirm(
+      `${newName} is already added to phonebook, replace the old number with a new one?`
+    )){
+      const updatedContact = {...personFound, number: newNumber}
+
+      personService
+        .update(personFound.id, updatedContact)
+        .then(returnedPerson => {
+          setContacts(contacts.map(person => 
+            person.name !== newName ? person : returnedPerson))
+        })
+    }
+
+    // Clear the input fields
+    // no matter what happened
+    setNewName('')
+    setNewNumber('')
+  }
+
+  const handleChange = (setChange) => (event) => {
+    setChange(event.target.value)
+  }
+
+  const deleteContact = (contact) => {
+    if ( !window.confirm(`Delete ${contact.name}?`) ) {
+      return
+    }
+
+    personService
+      .deleteEntry(contact.id)
+      .then(() => {
+        setContacts(contacts.filter(person => person.id !== contact.id))
+      })
+  }
+
+  const contactsToShow = contacts.filter(
+    contact => contact.name.toLowerCase().includes(filter.toLowerCase())
+  )
+
   return (
     <div>
-      phonebook-server
-    </div>
-  );
-}
+      <h2>Phonebook</h2>
 
+      <Filter filter={filter} handleChange={handleChange(setFilter)} />
+
+      <h3>Add a new</h3>
+
+      <PersonForm
+        handleSubmit={addContact}
+        name={newName}
+        handleName={handleChange(setNewName)}
+        number={newNumber}
+        handleNumber={handleChange(setNewNumber)}
+      />
+
+      <h3>Numbers</h3>
+
+      <Persons contacts={contactsToShow} handleDelete={deleteContact} />
+    </div>
+  )
+}
---
+++ round02/03-phonebook-server/src/your-solution/components.js
@@ -0,0 +1,38 @@
+export const Filter = ({filter, handleChange}) => (
+  <div>
+    filter shown with <input value={filter} onChange={handleChange} />
+  </div>
+)
+
+export const PersonForm = (props) => (
+  <form onSubmit={props.handleSubmit}>
+    <div>
+      name: <input value={props.name} onChange={props.handleName} />
+    </div>
+    <div>
+      number: <input value={props.number} onChange={props.handleNumber} />
+    </div>
+    <div>
+      <button type="submit">add</button>
+    </div>
+  </form>
+)
+
+export const Persons = ({contacts, handleDelete}) => (
+  contacts.map(person => 
+    <Person key={person.id} person={person} handleDelete={handleDelete} />
+  )
+)
+
+const Person = ({person, handleDelete}) => (
+  <div>
+    {person.name} {person.number} 
+    <Button handleClick={() => handleDelete(person)} />
+  </div>
+)
+
+const Button = ({handleClick}) => (
+  <button onClick={handleClick} style={{margin: 3}} >
+    delete
+  </button>
+)
