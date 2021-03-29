48c9659002f102e6f8f253e35610e1e265264d60
Timo Mäkinen <timo.makinen@tuni.fi>
2021-03-29 11:33:45 +0300

    add round03 / 02-bloglist-tests

---
+++ round03/02-bloglist-tests/src/reviews/solution-to-review-2/dummy-2.test.js
@@ -0,0 +1,4 @@
+
+// this file can be removed
+
+xtest('true is true (dummy test in review-2)', () => { expect(true).toBe(true) })
---
+++ round03/02-bloglist-tests/src/your-solution/components.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Blog = ({blog}) => (
+  <div>
+    {blog.title} {blog.author}
+  </div>  
+)
+
+
+export const BlogForm = () => (
+  <div>
+    BlogForm
+  </div>  
+)
