17ffe122ab7f1335ab83e3de9556fdbe72dfa3cd
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-04-03 23:03:42 +0300

    Add bloglist frontend step 1

--- round03/01-bloglist/src/your-solution/app.js
+++ round03/01-bloglist/src/your-solution/app.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import { Blog } from './components'
+import { Blog, LoginForm } from './components'
 import blogService from './services'
 
 // ------------------------------------------------------------ //
@@ -11,6 +11,9 @@ export const commitSHA = '-commit-sha-in-here-';
 
 export const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -18,9 +21,44 @@ export const App = () => {
     )
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const newLogin = await blogService.login({
+        username, password,
+      })
+      setUser(newLogin)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.log('eerrrroooor')
+      //setErrorMessage('wrong credentials')
+      //setTimeout(() => {
+      //  setErrorMessage(null)
+      //}, 5000)
+    }
+  }
+
+  if (user === null) {
+    return (
+      <div>
+        <h2>log in to application</h2>
+        <LoginForm
+          username={username}
+          password={password}
+          handleUsernameChange={({ target }) => setUsername(target.value)}
+          handlePasswordChange={({ target }) => setPassword(target.value)}
+          handleSubmit={handleLogin}
+        />
+      </div>
+    )
+  }
+
   return (
     <div>
       <h2>blogs</h2>
+      <p>{user.name} logged in</p>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
--- round03/01-bloglist/src/your-solution/components.js
+++ round03/01-bloglist/src/your-solution/components.js
@@ -1,7 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Blog = ({blog}) => (
   <div>
     {blog.title} {blog.author}
   </div>  
 )
+
+export const LoginForm = ({
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+    <div>
+      username
+        <input
+        type="text"
+        value={username}
+        name="Username"
+        onChange={handleUsernameChange}
+      />
+    </div>
+    <div>
+      password
+        <input
+        type="password"
+        value={password}
+        name="Password"
+        onChange={handlePasswordChange}
+      />
+    </div>
+    <button type="submit">login</button>
+    </form>
+  )
+}
