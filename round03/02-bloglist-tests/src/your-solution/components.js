51147fb84e79900a677d178560e9bafe23d6c773
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-04-07 10:14:38 +0300

    Add bloglist component tests steps 1-4

--- round03/02-bloglist-tests/src/your-solution/components.js
+++ round03/02-bloglist-tests/src/your-solution/components.js
@@ -1,14 +1,204 @@
-import React from 'react'
+import React, { useState, useImperativeHandle } from 'react'
+import PropTypes from 'prop-types'
 
-export const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
+export const Blog = ({ blog, handleLikeClick, user, handleDelete }) => {
+  const [showAll, setShowAll] = useState(false)
+
+  const toggleAll = { display: showAll ? '' : 'none' }
+  const buttonLabel = showAll ? 'hide' : 'view'
+
+  const removeButton = () => {
+    if (user.username === blog.user.username) {
+      return (
+        <Button
+          handleClick={() => handleDelete(blog)}
+          text="remove"
+        />
+      )
+    } else {
+      return null
+    }
+  }
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  //console.log(blog)
+
+  return (
+    <div style={blogStyle}>
+      <div>
+        {blog.title} {blog.author}
+        <Button
+          handleClick={() => setShowAll(!showAll)}
+          text={buttonLabel}
+        />
+      </div>
+      <div style={toggleAll} className="togglableContent">
+        <div>{blog.url}</div>
+        <div>
+          likes {blog.likes}
+          <Button
+            handleClick={() => handleLikeClick(blog)}
+            text="like"
+          />
+        </div>
+        <div>{blog.user.name}</div>
+        {removeButton()}
+      </div>
+    </div>
+  )
+}
+
+export const LoginForm = ({
+  handleSubmit,
+  handleUsernameChange,
+  handlePasswordChange,
+  username,
+  password
+}) => {
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        username
+        <input
+          type="text"
+          value={username}
+          name="Username"
+          onChange={handleUsernameChange}
+        />
+      </div>
+      <div>
+        password
+        <input
+          type="password"
+          value={password}
+          name="Password"
+          onChange={handlePasswordChange}
+        />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+}
+
+export const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick} style={{ margin: 3 }} >
+    {text}
+  </button>
 )
 
+export const BlogForm = ({ createBlog }) => {
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [url, setUrl] = useState('')
 
-export const BlogForm = () => (
-  <div>
-    BlogForm
-  </div>  
-)
+  const addBlog = (event) => {
+    event.preventDefault()
+    createBlog({
+      title: title,
+      author: author,
+      url: url,
+      likes: 0
+    })
+    setTitle('')
+    setAuthor('')
+    setUrl('')
+  }
+
+  return (
+    <div>
+      <h2>create new</h2>
+      <form onSubmit={addBlog}>
+        <div>
+          title:
+          <input
+            id='title'
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <div>
+          author:
+          <input
+            id='author'
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <div>
+          url:
+          <input
+            id='url'
+            type="url"
+            value={url}
+            onChange={({ target }) => setUrl(target.value)}
+          />
+        </div>
+        <button type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  const msgStyle = {
+    color: message.type === 'error' ? 'red' : 'green',
+    background: 'lightgrey',
+    fontSize: 20,
+    borderStyle: 'solid',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10
+  }
+
+  return (
+    <div style={msgStyle}>
+      {message.text}
+    </div>
+  )
+}
+
+export const Togglable = React.forwardRef((props, ref) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  useImperativeHandle(ref, () => {
+    return {
+      toggleVisibility
+    }
+  })
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        <button onClick={toggleVisibility}>{props.buttonLabel}</button>
+      </div>
+      <div style={showWhenVisible}>
+        {props.children}
+        <button onClick={toggleVisibility}>cancel</button>
+      </div>
+    </div>
+  )
+})
+
+Togglable.displayName = 'Togglable'
+
+Togglable.propTypes = {
+  buttonLabel: PropTypes.string.isRequired
+}
--- round03/02-bloglist-tests/src/your-solution/components.test.js
+++ round03/02-bloglist-tests/src/your-solution/components.test.js
@@ -13,18 +13,92 @@ describe(`\nCOMPONENT TESTS ${process.env.SOLUTION || 'your-solution'} [ ${commi
 
 
   describe('Blog', () => {
+    const user = {
+      name: 'Ned Flanders',
+      username: 'ned'
+    }
 
-    xtest('true is true (dummy test)', () => { expect(true).toBe(true) })
+    const blog = {
+      title: 'Easter Story',
+      author: 'Ned',
+      url: 'http://host/1',
+      likes: 1,
+      user: {name: 'Ned Flanders', username: 'ned'}
+    }
+
+    const mockHandler = jest.fn()
+
+    let component
+
+    beforeEach(() => {
+      component = render(
+        <Blog blog={blog} user={user} handleLikeClick={mockHandler}/>
+      )
+    })
+
+    test('renders title and author', () => {
+      const element = component.getByText('Easter Story Ned')
+      expect(element).toBeDefined()
+    })
+
+    test('the url and likes are not displayed by default', () => {
+      const div = component.container.querySelector('.togglableContent')
+  
+      expect(div).toHaveStyle('display: none')
+    })
+
+    test('after clicking the button, url and likes are displayed', () => {
+      const button = component.getByText('view')
+      fireEvent.click(button)
+  
+      const div = component.container.querySelector('.togglableContent')
+      expect(div).not.toHaveStyle('display: none')
+    })
+
+    test('clicking "like" twice calls event handler twice', () => {
+      const button = component.getByText('like')
+      fireEvent.click(button)
+      fireEvent.click(button)
+    
+      expect(mockHandler.mock.calls).toHaveLength(2)
+    })
 
   })
 
 
   describe('BlogForm', () => {
 
-    xtest('true is true (dummy test)', () => { expect(true).toBe(true) })
+    test('the form calls the event handler with the correct details', () => {
+      const createBlog = jest.fn()
+    
+      const component = render(
+        <BlogForm createBlog={createBlog} />
+      )
+    
+      const form = component.container.querySelector('form')
+      const title = component.container.querySelector('#title')
+      const author = component.container.querySelector('#author')
+      const url = component.container.querySelector('#url')
+        
+      fireEvent.change(title, { 
+        target: { value: 'Easter Story'}
+      })
+      fireEvent.change(author, { 
+        target: { value: 'Ned'}
+      })
+      fireEvent.change(url, { 
+        target: { value: 'http://host/1'}
+      })
+
+      fireEvent.submit(form)
+    
+      expect(createBlog.mock.calls).toHaveLength(1)
+      expect(createBlog.mock.calls[0][0].title).toBe('Easter Story')
+      expect(createBlog.mock.calls[0][0].author).toBe('Ned')
+      expect(createBlog.mock.calls[0][0].url).toBe('http://host/1')
+
+    })
 
   })
 
-
 })
-
