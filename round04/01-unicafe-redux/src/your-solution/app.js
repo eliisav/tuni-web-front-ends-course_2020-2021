fe545d86121c1d44f902f429cc20095abc42c143
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-04-17 22:30:02 +0300

    Add unicafe redux steps 1 and 2

--- round04/01-unicafe-redux/src/your-solution/app.js
+++ round04/01-unicafe-redux/src/your-solution/app.js
@@ -1,2 +1,25 @@
+import ReactDOM from 'react-dom'
+import { createStore } from 'redux'
+import reducer from './reducer'
 
-// replace this with the starter
+// ** enter commit sha of your repository in here **
+export const commitSHA = '-commit-sha-in-here-';
+
+
+export const store = createStore(reducer)
+
+
+export const App = () => {
+  return (
+    <div>
+      <button onClick={e => store.dispatch({type: 'GOOD'})}>good</button> 
+      <button onClick={e => store.dispatch({type: 'OK'})}>neutral</button> 
+      <button onClick={e => store.dispatch({type: 'BAD'})}>bad</button>
+      <button onClick={e => store.dispatch({type: 'ZERO'})}>reset stats</button>
+      <div>good {store.getState().good}</div>
+      <div>neutral {store.getState().ok}</div>
+      <div>bad {store.getState().bad}</div>
+    </div>
+  )
+}
+
---
+++ round04/01-unicafe-redux/src/your-solution/reducer.test.js
@@ -0,0 +1,94 @@
+import deepFreeze from 'deep-freeze'
+import counterReducer from './reducer'
+
+// *** enter commit sha of your repository in here ***
+const commitSHA = 'commit-sha-in-here'
+
+// *** do not remove or change this line ***
+describe(`\nUNIT TESTS ${process.env.SOLUTION || 'your-solution'} [ ${commitSHA} ]\n`, () => {
+
+
+  describe('unicafe reducer', () => {
+
+    const initialState = {
+      good: 0,
+      ok: 0,
+      bad: 0
+    }
+
+    test('should return a proper initial state when called with undefined state', () => {
+      const action = {
+        type: 'DO_NOTHING'
+      }
+
+      const newState = counterReducer(undefined, action)
+      expect(newState).toEqual(initialState)
+    })
+
+    test('good is incremented', () => {
+      const action = {
+        type: 'GOOD'
+      }
+      const state = initialState
+
+      deepFreeze(state)
+      const newState = counterReducer(state, action)
+      expect(newState).toEqual({
+        good: 1,
+        ok: 0,
+        bad: 0
+      })
+    })
+
+    test('neutral is incremented', () => {
+      const action = {
+        type: 'OK'
+      }
+      const state = initialState
+
+      deepFreeze(state)
+      const newState = counterReducer(state, action)
+      expect(newState).toEqual({
+        good: 0,
+        ok: 1,
+        bad: 0
+      })
+    })
+
+    test('bad is incremented', () => {
+      const action = {
+        type: 'BAD'
+      }
+      const state = initialState
+
+      deepFreeze(state)
+      const newState = counterReducer(state, action)
+      expect(newState).toEqual({
+        good: 0,
+        ok: 0,
+        bad: 1
+      })
+    })
+
+    test('reset stats resets all values to zero ', () => {
+      const action = {
+        type: 'ZERO'
+      }
+      const state = {
+        good: 3,
+        ok: 5,
+        bad: 2
+      }
+
+      deepFreeze(state)
+      const newState = counterReducer(state, action)
+      expect(newState).toEqual({
+        good: 0,
+        ok: 0,
+        bad: 0
+      })
+    })
+  })
+
+
+})
