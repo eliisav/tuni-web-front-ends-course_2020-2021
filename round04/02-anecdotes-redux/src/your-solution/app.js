d035aa04dd750660b9875495bdf355e32ee44aeb
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-04-17 23:38:49 +0300

    Add anecdotes redux step 1

--- round04/02-anecdotes-redux/src/your-solution/app.js
+++ round04/02-anecdotes-redux/src/your-solution/app.js
@@ -1,2 +1,44 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { useSelector, useDispatch } from 'react-redux'
+import reducer from './anecdoteReducer'
 
-// replace this with the starter
+export const store = createStore(reducer)
+
+// ** enter commit sha of your repository in here **
+export const commitSHA = '-commit-sha-in-here-';
+
+export const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch({
+      type: 'VOTE',
+      data: { id }
+    })
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form>
+        <div><input /></div>
+        <button>create</button>
+      </form>
+    </div>
+  )
+}
