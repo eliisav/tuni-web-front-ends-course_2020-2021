fcf5126f0eb6db324e5bb0520bb130b3ced8fc27
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-05-16 21:15:26 +0300

    Add library frontend stage 4

--- round06/02-library-frontend/src/your-solution/component-authors.js
+++ round06/02-library-frontend/src/your-solution/component-authors.js
@@ -1,9 +1,14 @@
-import React from 'react'
-import { useQuery } from '@apollo/client'
-import { ALL_AUTHORS } from './gql'
+import React, { useState } from 'react'
+import { useQuery, useMutation } from '@apollo/client'
+import { ALL_AUTHORS, UPDATE_AUTHOR } from './gql'
 
 const Authors = (props) => {
   const result = useQuery(ALL_AUTHORS)
+
+  const [name, setName] = useState('')
+  const [year, setYear] = useState('')
+  
+  const [ updateAuthor ] = useMutation(UPDATE_AUTHOR)
 
   if (!props.show) {
     return null
@@ -14,6 +19,16 @@ const Authors = (props) => {
   }
 
   const authors = result.data.allAuthors
+
+  const submit = async (event) => {
+    event.preventDefault()
+
+    const born = parseInt(year)
+    updateAuthor({  variables: { name, born } })
+
+    setName('')
+    setYear('')
+  }
 
   return (
     <div>
@@ -38,7 +53,26 @@ const Authors = (props) => {
           )}
         </tbody>
       </table>
-
+      
+      <h3>Set birthyear</h3>
+      <form onSubmit={submit}>
+        <div>
+          name
+          <input
+            value={name}
+            onChange={({ target }) => setName(target.value)}
+          />
+        </div>
+        <div>
+          born
+          <input
+            type='number'
+            value={year}
+            onChange={({ target }) => setYear(target.value)}
+          />
+        </div>
+        <button type='submit'>update author</button>
+      </form>
     </div>
   )
 }
