a157888861ecc89d4be9a8ec8416d917042c22e4
Timo Mäkinen <timo.makinen@tuni.fi>
2021-05-25 13:20:23 +0300

    add round07/03-course-with-types base code

---
+++ round07/03-course-with-types/src/reviews/solution-to-review-1/app.tsx
@@ -0,0 +1,4 @@
+
+// replace this with the file to be reviewed
+
+export default null;
---
+++ round07/03-course-with-types/src/your-solution/app.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+
+//import { Header, Content, Total } from './components';
+
+
+// ** enter commit sha of your repository in here **
+export const commitSHA = '-commit-sha-in-here-';
+
+export const App = () => {
+  const courseName = "Half Stack application development";
+  const courseParts = [
+    {
+      name: "Fundamentals",
+      exerciseCount: 10
+    },
+    {
+      name: "Using props to pass data",
+      exerciseCount: 7
+    },
+    {
+      name: "Deeper type usage",
+      exerciseCount: 14
+    }
+  ];
+
+  return (
+
+    <div>
+      <h1>{courseName}</h1>
+      <p>
+        {courseParts[0].name} {courseParts[0].exerciseCount}
+      </p>
+      <p>
+        {courseParts[1].name} {courseParts[1].exerciseCount}
+      </p>
+      <p>
+        {courseParts[2].name} {courseParts[2].exerciseCount}
+      </p>
+      <p>
+        Number of exercises{" "}
+        {courseParts.reduce((carry, part) => carry + part.exerciseCount, 0)}
+      </p>
+    </div>
+  );
+
+  // return (
+  //   <div>
+  //     <Header name={courseName} />
+  //     <Content ... />
+  //     <Total ... />
+  //   </div>
+  // )
+
+};
+
