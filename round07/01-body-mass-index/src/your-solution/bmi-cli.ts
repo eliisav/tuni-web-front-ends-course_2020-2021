0724229cbd4355c90439c98be2724cc43397d14c
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-05-30 00:28:36 +0300

    Add body mass index stage 2

--- round07/01-body-mass-index/src/your-solution/bmi-cli.ts
+++ round07/01-body-mass-index/src/your-solution/bmi-cli.ts
@@ -1,4 +1,29 @@
 
 import { calculateBmi } from './bmi-calculator';
 
+interface BmiValues {
+  height: number;
+  weight: number;
+}
 
+const parseArguments = (args: Array<string>): BmiValues => {
+  if (args.length < 4) throw new Error('Not enough arguments');
+  if (args.length > 4) throw new Error('Too many arguments');
+
+  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+    return {
+      height: Number(args[2]),
+      weight: Number(args[3])
+    };
+  } else {
+    throw new Error('Provided height and weight were not numbers!');
+  }
+};
+
+try {
+  const { height, weight } = parseArguments(process.argv);
+  console.log(calculateBmi(height, weight));
+} catch (e) {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+  console.log('Error, something bad happened, message: ', e.message);
+}
