822abb804fac419f2998ccc92ae8ca9219ceba9b
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-05-29 23:45:44 +0300

    Add body mass index stage 1

--- round07/01-body-mass-index/src/your-solution/bmi-calculator.ts
+++ round07/01-body-mass-index/src/your-solution/bmi-calculator.ts
@@ -1,7 +1,33 @@
 
+const calculateBmi = (height: number, weight: number) : string => {
+  if (height === 0) throw new Error('Can\'t divide by 0!');
 
-const calculateBmi = ()  => {
+  const bmi = weight/((height*height)/10000);
   
+  if (bmi < 15) {
+    return 'Very severely underweight';
+
+  } else if (bmi >= 15 && bmi < 16) {
+    return 'Severely underweight';
+
+  } else if (bmi >= 16 && bmi < 18.5) {
+    return 'Underweight';
+
+  } else if (bmi >= 18.5 && bmi < 25) {
+    return 'Normal (healthy weight)';
+
+  } else if (bmi >= 25 && bmi < 30) {
+    return 'Overweight';
+
+  } else if (bmi >= 30 && bmi < 35) {
+    return 'Obese Class I (Moderately obese)';
+
+  } else if (bmi >= 35 && bmi < 40) {
+    return 'Obese Class II (Severely obese)';
+
+  } else {
+    return 'Obese Class III (Very severely obese)';
+  }
 };
 
 
--- round07/01-body-mass-index/src/your-solution/bmi-hc.ts
+++ round07/01-body-mass-index/src/your-solution/bmi-hc.ts
@@ -1,3 +1,8 @@
 
 import { calculateBmi } from './bmi-calculator';
 
+try {
+  console.log(calculateBmi(180, 74));
+} catch (e) {
+  console.log('Something went wrong');
+}
