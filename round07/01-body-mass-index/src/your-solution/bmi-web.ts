de6a46912d413e59f20234cb5db40f0bbbb721f1
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-05-31 00:14:22 +0300

    Add body mass index stage 3

--- round07/01-body-mass-index/src/your-solution/bmi-web.ts
+++ round07/01-body-mass-index/src/your-solution/bmi-web.ts
@@ -4,4 +4,25 @@ import { calculateBmi } from './bmi-calculator';
 
 const app = express();
 
+app.get('/bmi', (req, res) => {
+  const height = Number(req.query.height);
+  const weight = Number(req.query.weight);
 
+  if (!isNaN(height) && !isNaN(weight)) {
+    try {
+      const bmi = calculateBmi(height, weight);
+      res.send({weight, height, bmi});
+    } catch (e) {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      res.status(500).send({ error: String(e.message) });
+    }
+  } else {
+    res.status(400).send({ error: "malformatted parameters" });
+  }
+});
+
+const PORT = 3003;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
