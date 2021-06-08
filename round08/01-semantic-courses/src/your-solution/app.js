13265780b314bbe2a5df071d53af32667bcd3b6e
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-06-08 23:12:27 +0300

    Add semantic courses

--- round08/01-semantic-courses/src/your-solution/app.js
+++ round08/01-semantic-courses/src/your-solution/app.js
@@ -1,6 +1,9 @@
-
+import React from 'react';
 import { Course } from './components';
 import courses from './courses-data';
+import 'semantic-ui-css/semantic.min.css';
+import { Container, Divider, Header, Segment } from 'semantic-ui-react';
+
 
 // ** ENTER COMMIT SHA OF YOUR REPO IN HERE **
 export const commitSHA = '-commit-sha-in-here-';
@@ -8,11 +11,18 @@ export const commitSHA = '-commit-sha-in-here-';
 export const App = () => {
 
   return (
-    <>
-      <h1>Web development curriculum</h1>
+    <Container textAlign='center'>
+
+      <Segment padded='very'>
+        <Header as='h1' color='grey'>Web development curriculum</Header>
+      </Segment>
+
+      <Divider />
+
       {courses.map(course =>
         <Course key={course.id} course={course} />
       )}
-    </>
+
+    </Container>
   )
 }
--- round08/01-semantic-courses/src/your-solution/components.js
+++ round08/01-semantic-courses/src/your-solution/components.js
@@ -1,33 +1,38 @@
+import { Header, Icon, Label, Table } from 'semantic-ui-react';
 
-const CourseHeader = ({ course }) =>
-  <h2>
-    {course}
-  </h2>
-
-
-const Content = ({ parts }) => {
+const CourseHeader = ({ course }) => {
+  const iconName = course === "Node.js" ? 'node' : 'code'
   return (
-    <>
-      {parts.map(part =>
-        <Part key={part.id} part={part} />)
-      }
-    </>
+    <Header as='h2'>
+      <Icon name={iconName} />
+      <Header.Content>{course}</Header.Content>
+    </Header>
   )
 }
 
+const Content = ({ parts }) => {
+  return (
+    <Table.Body>
+      {parts.map(part =>
+        <Part key={part.id} part={part} />)
+      }
+    </Table.Body>
+  )
+}
 
 const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
-
+  <Table.Row>
+    <Table.Cell width='10'>{part.name}</Table.Cell>
+    <Table.Cell>{part.exercises}</Table.Cell>
+  </Table.Row>
 
 const Total = ({ parts }) => {
   const total = parts.reduce((acc, cur) => acc + cur.exercises, 0)
   return (
-    <p>
-      total of {total} exercises
-    </p>
+    <Table.Row>
+      <Table.HeaderCell><Label ribbon>Exercises total</Label></Table.HeaderCell>
+      <Table.HeaderCell>{total}</Table.HeaderCell>
+    </Table.Row>
   )
 }
 
@@ -35,10 +40,23 @@ export const Course = ({ course }) => {
   return (
     <>
       <CourseHeader course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts}
-      />
+
+      <Table celled>
+
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>Part</Table.HeaderCell>
+            <Table.HeaderCell>Exercises</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        
+        <Content parts={course.parts} />
+        
+        <Table.Footer>
+          <Total parts={course.parts}/>
+        </Table.Footer>
+
+      </Table>
     </>
   )
 }
-
