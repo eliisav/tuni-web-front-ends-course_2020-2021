4813a57d093160c682b45eacac0b1e3f1f818b48
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-06-13 20:01:39 +0300

    Remove the segment and use a divider instead

--- round08/01-semantic-courses/src/your-solution/app.js
+++ round08/01-semantic-courses/src/your-solution/app.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Course } from './components';
 import courses from './courses-data';
 import 'semantic-ui-css/semantic.min.css';
-import { Container, Divider, Header, Segment } from 'semantic-ui-react';
+import { Container, Divider, Header } from 'semantic-ui-react';
 
 
 // ** ENTER COMMIT SHA OF YOUR REPO IN HERE **
@@ -11,11 +11,11 @@ export const commitSHA = '-commit-sha-in-here-';
 export const App = () => {
 
   return (
-    <Container textAlign='center'>
+    <Container text textAlign='center'>
 
-      <Segment padded='very'>
-        <Header as='h1' color='grey'>Web development curriculum</Header>
-      </Segment>
+      <Divider hidden />
+
+      <Header as='h1' color='grey'>Web development curriculum</Header>
 
       <Divider />
 
