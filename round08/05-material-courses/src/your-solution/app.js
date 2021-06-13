e91628575c870f0acbf19aafef41607feb08b425
Eliisa Väkevä <eliisa.vakeva@tuni.fi>
2021-06-13 19:03:13 +0300

    Add material courses

--- round08/05-material-courses/src/your-solution/app.js
+++ round08/05-material-courses/src/your-solution/app.js
@@ -2,17 +2,30 @@
 import { Course } from './components';
 import courses from './courses-data';
 
+import { Box, Container, Divider, Typography} from '@material-ui/core';
+import React from 'react';
+
+
 // ** ENTER COMMIT SHA OF YOUR REPO IN HERE **
 export const commitSHA = '-commit-sha-in-here-';
 
 export const App = () => {
 
   return (
-    <>
-      <h1>Web development curriculum</h1>
-      {courses.map(course =>
-        <Course key={course.id} course={course} />
-      )}
-    </>
+      <Container maxWidth="md">
+        <Box my={4}>
+
+          <Typography variant="h4" component="h1" align="center">
+            Web development curriculum
+          </Typography>
+
+          <Divider />
+
+          {courses.map(course =>
+            <Course key={course.id} course={course} />
+          )}
+          
+        </Box>
+      </Container>
   )
 }
--- round08/05-material-courses/src/your-solution/components.js
+++ round08/05-material-courses/src/your-solution/components.js
@@ -1,44 +1,83 @@
+import { Code, DeveloperMode } from '@material-ui/icons'
+import {
+  Box,
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableFooter,
+  TableHead,
+  TableRow,
+  Typography
+} from '@material-ui/core'
 
-const CourseHeader = ({ course }) =>
-  <h2>
-    {course}
-  </h2>
 
+const Icon = ({ course }) => (
+  course === "Node.js" ? <Code /> : <DeveloperMode />
+)
+
+const CourseHeader = ({ course }) => {
+  const iconName = course === "Node.js" ? 'code' : 'code'
+  return (
+    <Typography variant="h5" component="h2">
+      <Icon course={course} />
+      {course}
+    </Typography>
+  )
+}
 
 const Content = ({ parts }) => {
   return (
-    <>
+    <TableBody>
       {parts.map(part =>
         <Part key={part.id} part={part} />)
       }
-    </>
+    </TableBody>
   )
 }
 
 
 const Part = ({ part }) =>
-  <p>
-    {part.name} {part.exercises}
-  </p>
+  <TableRow>
+    <TableCell>{part.name}</TableCell>
+    <TableCell align="right">{part.exercises}</TableCell>
+  </TableRow>
 
 
 const Total = ({ parts }) => {
   const total = parts.reduce((acc, cur) => acc + cur.exercises, 0)
   return (
-    <p>
-      total of {total} exercises
-    </p>
+    <TableRow>
+      <TableCell>Exercises total</TableCell>
+      <TableCell align="right">{total}</TableCell>
+    </TableRow>
   )
 }
+
 
 export const Course = ({ course }) => {
   return (
-    <>
+    <Box my={4}>
       <CourseHeader course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts}
-      />
-    </>
+      <TableContainer component={Paper}>
+        <Table>
+
+          <TableHead>
+            <TableRow>
+              <TableCell>Part</TableCell>
+              <TableCell align="right">Exercises</TableCell>
+            </TableRow>
+          </TableHead>
+
+          <Content parts={course.parts} />
+
+          <TableFooter>
+            <Total parts={course.parts} />
+          </TableFooter>
+
+        </Table>
+      </TableContainer>
+    </Box>
   )
 }
-
